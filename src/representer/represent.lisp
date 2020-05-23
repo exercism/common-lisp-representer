9afb6828f39f43f2803e6dfbe99bc36415bd7b4f
Mark Simpson <verdammelt@gmail.com>
2020-05-23 16:40:48 -0400

    implement handling for defpackage.

--- src/representer/represent.lisp
+++ src/representer/represent.lisp
@@ -2,10 +2,7 @@
 
 (defun represent-toplevel (slug forms)
   (placeholder:init slug)
-  (values (reduce
-           #'(lambda (repr form) (append (represent (car form) form) repr))
-           forms
-           :initial-value (list))
+  (values (loop for form in forms collect (represent (car form) form))
           (placeholder:->alist)))
 
 (defgeneric represent (symbol form))
@@ -13,3 +10,14 @@
 (defmethod represent (symbol form)
   (declare (ignore symbol))
   form)
+
+(defmethod represent ((symbol (eql 'defpackage)) form)
+  (let ((package-name (second form))
+        (body (cddr form)))
+    `(,symbol ,(placeholder:add package-name)
+              ,@(sort (mapcar #'(lambda (f) (represent (car f) f)) body)
+                      #'string<
+                      :key #'(lambda (f) (symbol-name (car f)))))))
+
+(defmethod represent ((symbol (eql :export)) form)
+  `(,symbol ,@(mapcar #'placeholder:add (rest form))))
--- test/represent.lisp
+++ test/represent.lisp
@@ -4,5 +4,16 @@
 (in-suite represent-test)
 
 (test empty-form
-  (is (equalp (multiple-value-list (representer::represent-toplevel "slug" '()))
-              '(() ()))))
+  (is (equalp '(() ())
+              (multiple-value-list (representer::represent-toplevel "slug" '())))))
+
+(test defpackage
+  (is (equalp '(((defpackage :test-0
+                   (:export :test-1)
+                   (:use :cl)))
+                ((":TEST-0" . "#:PACK") (":TEST-1" . "#:THINGIE")))
+              (multiple-value-list (representer::represent-toplevel
+                                    "test"
+                                    '((defpackage #:pack
+                                        (:use :cl)
+                                        (:export #:thingie))))))))
