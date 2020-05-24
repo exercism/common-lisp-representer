2382e369f02719cc8de497f62d975f4ba3de5a46
Mark Simpson <verdammelt@gmail.com>
2020-05-24 13:47:52 -0400

    package shenanigans to get nice symbols.

--- src/representer/main.lisp
+++ src/representer/main.lisp
@@ -2,9 +2,10 @@
 
 (defun slurp-solution (slug directory)
   "Reads in the forms from the file 'SLUG.lisp' in DIRECTORY."
-  (uiop:with-safe-io-syntax ()
-    (uiop:read-file-forms
-     (merge-pathnames (make-pathname :name slug :type "lisp") directory)))  )
+  (let ((package-name (package-name *package*)))
+   (uiop:with-safe-io-syntax (:package package-name)
+     (uiop:read-file-forms
+      (merge-pathnames (make-pathname :name slug :type "lisp") directory))))  )
 
 (defun write-representation (forms directory)
   "Writes the FORMS to a file 'representation.txt' in the DIRECTORY."
@@ -20,9 +21,15 @@
       (yason:encode-alist symbol-map output))))
 
 (defun main (args)
-  (let ((slug (first args))
-        (directory (pathname (second args))))
-    (multiple-value-bind (forms symbol-map)
-        (represent-toplevel slug (slurp-solution slug directory))
-      (write-representation forms directory)
-      (write-symbol-map symbol-map directory))))
+  "ARGS is '(SLUG SOLUTION-DIRECTORY)"
+  (let* ((slug (first args))
+         (directory (pathname (second args)))
+         (repr-package-name (format nil "~:@(~A~)-REPR" slug)))
+    (when (find-package repr-package-name) (delete-package repr-package-name))
+    (unwind-protect
+         (let ((*package* (make-package repr-package-name :use '(:cl))))
+           (multiple-value-bind (forms symbol-map)
+               (represent-toplevel slug (slurp-solution slug directory))
+             (write-representation forms directory)
+             (write-symbol-map symbol-map directory)))
+      (delete-package repr-package-name))))
