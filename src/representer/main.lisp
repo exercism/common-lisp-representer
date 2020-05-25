4a4ba6357a3c9905081f3149ee4c91ea1b2c4e2c
Mark Simpson <verdammelt@gmail.com>
2020-05-25 10:22:50 -0400

    big refactor to let more code be testable
    Now MAIN is a much smaller function and is the only untested chunk.

--- src/representer/main.lisp
+++ src/representer/main.lisp
@@ -1,36 +1,44 @@
 (in-package #:representer)
 
-(defun slurp-solution (slug directory)
-  "Reads in the forms from the file 'SLUG.lisp' in DIRECTORY."
+(defun solution-file (slug directory)
+  (merge-pathnames (make-pathname :name slug :type "lisp") directory))
+(defun representation-file (directory)
+  (merge-pathnames (make-pathname :name "representation" :type "txt") directory))
+(defun mapping-file (directory)
+  (merge-pathnames (make-pathname :name "mapping" :type "json") directory))
+(defun repr-package-name (slug) (format nil "~:@(~A~)-REPR" slug))
+
+(defun kill-package (package-name) (ignore-errors (delete-package package-name)))
+
+(defun slurp-solution (stream)
   (let ((package-name (package-name *package*)))
-   (uiop:with-safe-io-syntax (:package package-name)
-     (uiop:read-file-forms
-      (merge-pathnames (make-pathname :name slug :type "lisp") directory))))  )
+    (uiop:with-safe-io-syntax (:package package-name)
+      (uiop:slurp-stream-forms stream))))
 
-(defun write-representation (forms directory)
-  "Writes the FORMS to a file 'representation.txt' in the DIRECTORY."
-  (let ((output-file (merge-pathnames (make-pathname :name "representation" :type "txt") directory)))
-    (with-open-file (output output-file :direction :output :if-exists :supersede)
-      (write forms :stream output))))
+(defun write-repr (repr stream) (write repr :stream stream))
+(defun write-mapping (mapping stream) (yason:encode-alist mapping stream))
 
-(defun write-symbol-map (symbol-map directory)
-  "Writes SYMBOL-MAP (a list of string typed key-value pairs to a JSON file
-'mapping.json' in the DIRECTORY."
-  (let ((output-file (merge-pathnames (make-pathname :name "mapping" :type "json") directory)))
-    (with-open-file (output output-file :direction :output :if-exists :supersede)
-      (yason:encode-alist symbol-map output))))
-
-(defun main (args)
-  "ARGS is '(SLUG SOLUTION-DIRECTORY)"
-  (let* ((slug (first args))
-         (directory (pathname (second args)))
-         (repr-package-name (format nil "~:@(~A~)-REPR" slug)))
-    (when (find-package repr-package-name) (delete-package repr-package-name))
+(defun produce-representation (slug
+                               solution-stream
+                               repr-stream
+                               mapping-stream)
+  (let ((package-name (repr-package-name slug)))
+    (kill-package package-name)
     (unwind-protect
-         (let ((*package* (make-package repr-package-name :use '(:cl))))
+         (let ((*package* (make-package package-name :use '(:cl))))
            (placeholder:init slug)
-           (let ((forms (represent nil (slurp-solution slug directory)))
-                 (symbol-map (placeholder:->alist)))
-             (write-representation forms directory)
-             (write-symbol-map symbol-map directory)))
-      (delete-package repr-package-name))))
+           (write-repr (represent nil (slurp-solution solution-stream))
+                       repr-stream)
+           (write-mapping (placeholder:->alist) mapping-stream))
+      (kill-package package-name))))
+
+
+(defun main (&rest args)
+  (destructuring-bind ((slug directory)) args
+    (with-open-file (solution-stream (solution-file slug directory)
+                                     :direction :input)
+      (with-open-file (repr-stream (representation-file directory)
+                                   :direction :output)
+        (with-open-file (mapping-stream (mapping-file directory)
+                                        :direction :output)
+          (produce-representation slug solution-stream repr-stream mapping-stream))))))
--- test/end-to-end.lisp
+++ test/end-to-end.lisp
@@ -3,16 +3,37 @@
 (def-suite end-to-end :in all-tests)
 (in-suite end-to-end)
 
-(defun read-test-file (slug type)
-  (uiop:read-file-forms (make-pathname :directory '(:relative "test" "files")
-                                       :name slug
-                                       :type type)))
+(defun load-expected-values (directory)
+  (let ((repr-file (representer::representation-file directory))
+        (mapping-file (representer::mapping-file directory)))
+    (values (uiop:read-file-forms repr-file)
+            (yason:parse (uiop:read-file-string mapping-file)))))
+
+(defun get-actual-values (slug directory)
+  (let ((actual-repr-stream (make-string-output-stream))
+        (actual-mapping-stream (make-string-output-stream))
+        (solution-file (representer::solution-file slug directory)))
+
+    (with-open-file (solution-stream solution-file :direction :input)
+      (representer::produce-representation
+       slug solution-stream
+       actual-repr-stream actual-mapping-stream))
+
+    (let ((actual-repr-str (get-output-stream-string actual-repr-stream))
+          (actual-mapping-str (get-output-stream-string actual-mapping-stream)))
+      (values (uiop:slurp-stream-forms
+               (make-string-input-stream actual-repr-str))
+              (yason:parse actual-mapping-str)))))
 
 (test two-fer
   (let* ((slug "two-fer")
-         (solution (read-test-file slug "lisp"))
-         (expected (read-test-file slug "repr")))
-    (placeholder:init slug)
-    (let ((actual (list (representer::represent nil solution)
-                        (placeholder:->alist))))
-      (is (equalp++ expected actual)))))
+         (directory (make-pathname :directory (list :relative "test" "files" slug))))
+
+    (multiple-value-bind (expected-repr expected-mapping)
+        (load-expected-values directory)
+
+      (multiple-value-bind (actual-repr actual-mapping)
+          (get-actual-values slug directory)
+
+        (is (equalp++ expected-repr actual-repr))
+        (is (equalp++ expected-mapping actual-mapping))))))
