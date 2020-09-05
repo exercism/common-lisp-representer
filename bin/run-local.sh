c6b32b170b7ff8d484c593f7274e4e29e45b9237
Mark Simpson <verdammelt@gmail.com>
2020-09-05 15:50:50 -0400

    interface now defines three arguments: slug, input-dir and output-dir

--- bin/run-local.sh
+++ bin/run-local.sh
@@ -1,15 +1,17 @@
 #!/bin/sh -e
 
 slug=$1
-local_dir=`realpath $2`
+input_dir=`realpath $2`
+output_dir=`realpath $3`
 
 image=`docker build -q .`
 
 docker run \
        --network none \
        --read-only \
-       --mount type=bind,source=${local_dir},target=/dir\
+       --mount type=bind,source=${input_dir},target=/input\
+       --mount type=bind,source=${output_dir},target=/output\
        --rm \
-       -it $image $slug /dir/
+       -it $image $slug /input/ /output/
 
 docker image rm $image
